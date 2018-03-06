1aff9e066ac480a9c11d622e223d2ebf17ac10e9
刚 <984805129@qq.com>
2018-03-06 10:26:12 +0800

    es6块级作用域

---
+++ BlockScope.js
@@ -0,0 +1,9 @@
+for(var i=0; i<5; i++){
+    console.log(i); //0 1 2 3 4
+}
+console.log(i); //5
+
+for(let j=0; j<5; j++){
+    console.log(j); //0 1 2 3 4
+}
+//console.log(j); //ReferenceError: j is not defined
